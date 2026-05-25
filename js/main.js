c57d2916ca7167d0a79bffbe2a31d17fcf51daee
gamza <rachu970605@gmail.com>
2026-05-26 08:26:35 +0900

    인기 검색어 루프 구현, 접근성 점검

---
+++ js/main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    mainView.popularkeyword();
+});
+
+const mainView = {
+    writeBtn() {
+
+    },
+
+    popularkeyword() {
+        const list = document.querySelector(".rank-list");
+        const items = document.querySelectorAll(".rank-list li");
+
+        const itemHeight = items[0].offsetHeight;
+
+        let index = 0;
+
+        setInterval(() => {
+            index++;
+
+            gsap.to(list, {
+                y: -(index * itemHeight),
+                duration: 0.4,
+
+                onComplete: () => {
+                    // 마지막이면 리셋
+                    if (index === (items.length - 1)) {
+                        gsap.set(list, { y: 0 });
+                        index = 0;
+                    }
+                }
+            });
+        }, 2000);
+    }
+
+};
